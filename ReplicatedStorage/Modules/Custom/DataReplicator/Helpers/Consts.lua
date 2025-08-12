191a039ae52edd02a703326604855a5a0ee68ea4
shinny77 <john.shin@attaliscapital.com>
2025-08-12 19:24:57 +1000

    Updated ReplicatedStorage/Modules/Custom/DataReplicator/Helpers/Consts.lua

---
+++ ReplicatedStorage/Modules/Custom/DataReplicator/Helpers/Consts.lua
@@ -0,0 +1,25 @@
+-- @ScriptType: ModuleScript
+--[[
+This is associated with DataReplicator package module.
+Formed by :- Dilpreet Singh at ChicMic Studios @2024
+]]
+
+local Consts = {}
+
+
+
+
+
+--===========================********************** Data Stores ETC ******************============================
+
+--Consts.DataStoreVersions = {
+--	{
+--		GameDataStoreVersion = 0.1,
+--	},
+--}
+--table.freeze(Consts.DataStoreVersions)
+
+--===========================********************** Data Stores ETC ******************============================
+
+
+return Consts
