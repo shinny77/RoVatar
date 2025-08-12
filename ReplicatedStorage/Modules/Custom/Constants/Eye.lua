8c64256b9c7c026e4fdaf439e2e350edf27abb83
shinny77 <john.shin@attaliscapital.com>
2025-08-12 19:25:30 +1000

    Updated ReplicatedStorage/Modules/Custom/Constants/Eye.lua

---
+++ ReplicatedStorage/Modules/Custom/Constants/Eye.lua
@@ -0,0 +1,206 @@
+-- @ScriptType: ModuleScript
+return function(Constants)
+	return {
+		-- Inventory
+		Eye_01 = {
+			-- Item Info 
+			Id = "Eye_01",
+			Name = "Eye_01",
+			Description = " ",
+			Image = "http://www.roblox.com/asset/?id=2818165600",
+			CurrencyType = Constants.CurrencyTypes.Gems,
+			InventoryType = Constants.InventoryType.Styling,
+			ItemType = Constants.ItemType.Eye,
+		},
+		Eye_02 = {
+			-- Item Info 
+			Id = "Eye_02",
+			Name = "Eye_02",
+			Description = " ",
+			Image = "http://www.roblox.com/asset/?id=2801605956",
+			CurrencyType = Constants.CurrencyTypes.Gems,
+			InventoryType = Constants.InventoryType.Styling,
+			ItemType = Constants.ItemType.Eye,
+		},
+		Eye_03 = {
+			-- Item Info 
+			Id = "Eye_03",
+			Name = "Eye_03",
+			Description = " ",
+			Image = "http://www.roblox.com/asset/?id=2818161064",
+			CurrencyType = Constants.CurrencyTypes.Gems,
+			InventoryType = Constants.InventoryType.Styling,
+			ItemType = Constants.ItemType.Eye,
+		},
+		Eye_04 = {
+			-- Item Info 
+			Id = "Eye_04",
+			Name = "Eye_04",
+			Description = " ",
+			Image = "http://www.roblox.com/asset/?id=2818161566",
+			CurrencyType = Constants.CurrencyTypes.Gems,
+			InventoryType = Constants.InventoryType.Styling,
+			ItemType = Constants.ItemType.Eye,
+		},
+		Eye_05 = {
+			-- Item Info 
+			Id = "Eye_05",
+			Name = "Eye_05",
+			Description = " ",
+			Image = "http://www.roblox.com/asset/?id=2801594656",
+			CurrencyType = Constants.CurrencyTypes.Gems,
+			InventoryType = Constants.InventoryType.Styling,
+			ItemType = Constants.ItemType.Eye,
+		},
+		Eye_06 = {
+			-- Item Info 
+			Id = "Eye_06",
+			Name = "Eye_06",
+			Description = " ",
+			Image = "http://www.roblox.com/asset/?id=2801605712",
+			CurrencyType = Constants.CurrencyTypes.Gems,
+			InventoryType = Constants.InventoryType.Styling,
+			ItemType = Constants.ItemType.Eye,
+		},
+		Eye_07 = {
+			-- Item Info 
+			Id = "Eye_07",
+			Name = "Eye_07",
+			Description = " ",
+			Image = "http://www.roblox.com/asset/?id=2801605125",
+			CurrencyType = Constants.CurrencyTypes.Gems,
+			InventoryType = Constants.InventoryType.Styling,
+			ItemType = Constants.ItemType.Eye,
+		},
+		Eye_08 = {
+			-- Item Info 
+			Id = "Eye_08",
+			Name = "Eye_08",
+			Description = " ",
+			Image = "http://www.roblox.com/asset/?id=2818168785",
+			CurrencyType = Constants.CurrencyTypes.Gems,
+			InventoryType = Constants.InventoryType.Styling,
+			ItemType = Constants.ItemType.Eye,
+		},
+		Eye_09 = {
+			-- Item Info 
+			Id = "Eye_09",
+			Name = "Eye_09",
+			Description = " ",
+			Image = "http://www.roblox.com/asset/?id=2818166000",
+			CurrencyType = Constants.CurrencyTypes.Gems,
+			InventoryType = Constants.InventoryType.Styling,
+			ItemType = Constants.ItemType.Eye,
+		},
+		Eye_10 = {
+			-- Item Info 
+			Id = "Eye_10",
+			Name = "Eye_10",
+			Description = " ",
+			Image = "http://www.roblox.com/asset/?id=2801605473",
+			CurrencyType = Constants.CurrencyTypes.Gems,
+			InventoryType = Constants.InventoryType.Styling,
+			ItemType = Constants.ItemType.Eye,
+		},
+		Eye_11 = {
+			-- Item Info 
+			Id = "Eye_11",
+			Name = "Eye_11",
+			Description = " ",
+			Image = "http://www.roblox.com/asset/?id=2818161322",
+			CurrencyType = Constants.CurrencyTypes.Gems,
+			InventoryType = Constants.InventoryType.Styling,
+			ItemType = Constants.ItemType.Eye,
+		},
+		Eye_12 = {
+			-- Item Info 
+			Id = "Eye_12",
+			Name = "Eye_12",
+			Description = " ",
+			Image = "http://www.roblox.com/asset/?id=2818168556",
+			CurrencyType = Constants.CurrencyTypes.Gems,
+			InventoryType = Constants.InventoryType.Styling,
+			ItemType = Constants.ItemType.Eye,
+		},
+		Eye_13 = {
+			-- Item Info 
+			Id = "Eye_13",
+			Name = "Eye_13",
+			Description = " ",
+			Image = "http://www.roblox.com/asset/?id=2818165738",
+			CurrencyType = Constants.CurrencyTypes.Gems,
+			InventoryType = Constants.InventoryType.Styling,
+			ItemType = Constants.ItemType.Eye,
+		},
+		Eye_14 = {
+			-- Item Info 
+			Id = "Eye_14",
+			Name = "Eye_14",
+			Description = " ",
+			Image = "http://www.roblox.com/asset/?id=2801595347",
+			CurrencyType = Constants.CurrencyTypes.Gems,
+			InventoryType = Constants.InventoryType.Styling,
+			ItemType = Constants.ItemType.Eye,
+		},
+		Eye_15 = {
+			-- Item Info 
+			Id = "Eye_15",
+			Name = "Eye_15",
+			Description = " ",
+			Image = "http://www.roblox.com/asset/?id=2801594973",
+			CurrencyType = Constants.CurrencyTypes.Gems,
+			InventoryType = Constants.InventoryType.Styling,
+			ItemType = Constants.ItemType.Eye,
+		},
+		Eye_16 = {
+			-- Item Info 
+			Id = "Eye_16",
+			Name = "Eye_16",
+			Description = " ",
+			Image = "http://www.roblox.com/asset/?id=3523957747",
+			CurrencyType = Constants.CurrencyTypes.Gems,
+			InventoryType = Constants.InventoryType.Styling,
+			ItemType = Constants.ItemType.Eye,
+		},
+		Eye_17 = {
+			-- Item Info 
+			Id = "Eye_17",
+			Name = "Eye_17",
+			Description = " ",
+			Image = "http://www.roblox.com/asset/?id=3523956852",
+			CurrencyType = Constants.CurrencyTypes.Gems,
+			InventoryType = Constants.InventoryType.Styling,
+			ItemType = Constants.ItemType.Eye,
+		},
+		Eye_18 = {
+			-- Item Info 
+			Id = "Eye_18",
+			Name = "Eye_18",
+			Description = " ",
+			Image = "http://www.roblox.com/asset/?id=2801584953",
+			CurrencyType = Constants.CurrencyTypes.Gems,
+			InventoryType = Constants.InventoryType.Styling,
+			ItemType = Constants.ItemType.Eye,
+		},
+		Eye_19 = {
+			-- Item Info 
+			Id = "Eye_19",
+			Name = "Eye_19",
+			Description = " ",
+			Image = "http://www.roblox.com/asset/?id=2801584512",
+			CurrencyType = Constants.CurrencyTypes.Gems,
+			InventoryType = Constants.InventoryType.Styling,
+			ItemType = Constants.ItemType.Eye,
+		},
+		Eye_20 = {
+			-- Item Info 
+			Id = "Eye_20",
+			Name = "Eye_20",
+			Description = " ",
+			Image = "http://www.roblox.com/asset/?id=2801585179",
+			CurrencyType = Constants.CurrencyTypes.Gems,
+			InventoryType = Constants.InventoryType.Styling,
+			ItemType = Constants.ItemType.Eye,
+		},
+	}
+end
