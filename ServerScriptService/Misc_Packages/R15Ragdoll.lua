f475e4ffd7b19421e185279f7cea51fc141a7ae4
shinny77 <john.shin@attaliscapital.com>
2025-08-12 19:33:15 +1000

    Updated ServerScriptService/Misc_Packages/R15Ragdoll.lua

---
+++ ServerScriptService/Misc_Packages/R15Ragdoll.lua
@@ -0,0 +1,8 @@
+-- @ScriptType: Script
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local StarterPlayer = game:GetService("StarterPlayer")
+
+script.buildRagdoll.Parent = ReplicatedStorage
+script.RagdollHandler.Parent = ReplicatedStorage
+script.Ragdoll_Client.Parent = StarterPlayer.StarterPlayerScripts
