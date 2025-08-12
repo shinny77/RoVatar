5677efc6aefb47668313b67625b09c057328da65
shinny77 <john.shin@attaliscapital.com>
2025-08-12 19:33:21 +1000

    Updated ServerScriptService/Misc_Packages/R15Ragdoll/Ragdoll_Server.lua

---
+++ ServerScriptService/Misc_Packages/R15Ragdoll/Ragdoll_Server.lua
@@ -0,0 +1,6 @@
+-- @ScriptType: Script
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local RagdollHandler = ReplicatedStorage:WaitForChild("RagdollHandler")
+
+require(RagdollHandler)
