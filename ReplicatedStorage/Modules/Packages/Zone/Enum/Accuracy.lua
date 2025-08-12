9b9db83747b72af1d8cfd14dd98ea7cd163065a7
shinny77 <john.shin@attaliscapital.com>
2025-08-12 19:27:48 +1000

    Updated ReplicatedStorage/Modules/Packages/Zone/Enum/Accuracy.lua

---
+++ ReplicatedStorage/Modules/Packages/Zone/Enum/Accuracy.lua
@@ -0,0 +1,9 @@
+-- @ScriptType: ModuleScript
+-- enumName, enumValue, additionalProperty
+-- in this case, additionalProperty is the delay between each check
+return {
+	{"Low", 	1,	1.0},
+	{"Medium",	2,	0.5},
+	{"High",	3,	0.1},
+	{"Precise",	4,	0.0},
+}
