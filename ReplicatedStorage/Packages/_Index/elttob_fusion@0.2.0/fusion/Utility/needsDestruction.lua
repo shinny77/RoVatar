8c68174e8e7ea72eb1d05ef2fce37892966f6e88
shinny77 <john.shin@attaliscapital.com>
2025-08-12 19:31:02 +1000

    Updated ReplicatedStorage/Packages/_Index/elttob_fusion@0.2.0/fusion/Utility/needsDestruction.lua

---
+++ ReplicatedStorage/Packages/_Index/elttob_fusion@0.2.0/fusion/Utility/needsDestruction.lua
@@ -0,0 +1,13 @@
+-- @ScriptType: ModuleScript
+--!strict
+
+--[[
+    Returns true if the given value is not automatically memory managed, and
+    requires manual cleanup.
+]]
+
+local function needsDestruction(x: any): boolean
+    return typeof(x) == "Instance"
+end
+
+return needsDestruction
