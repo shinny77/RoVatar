d4123c4326dfe583df6b2e13b2bef1ef0af103b7
shinny77 <john.shin@attaliscapital.com>
2025-08-13 20:52:27 +1000

    Updated Workspace/Note.lua

---
+++ Workspace/Note.lua
@@ -0,0 +1,19 @@
+-- @ScriptType: ModuleScript
+--[[
+	--*> Strength[MANA]
+	* Handle on client Side. So, Don't use Value on server Side.
+	
+
+	--* Development changes
+	* Change SFX assetsID from SFX script Child -> SFXHandler
+	* Change Costs [Stamina Speed, XP Distribution, Damages, Cooldown] From Costs Script
+]]
+
+
+--[[
+For Tutorial 
+-> Delete All maps except kiyoshi island
+-> Setup Guru pathik as TutorialGuider tag
+-> Delete all Npc and remain Earth Binder
+-> Update QuestController and QuestGui script in Tutorial 
+]]
